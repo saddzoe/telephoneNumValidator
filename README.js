4ec779ce5405133a6359ec2c67b1ebf8fad97bd6
saddzoe <73001138+saddzoe@users.noreply.github.com>
2021-02-15 22:15:56 -0500

    Update README.js

--- README.js
+++ README.js
@@ -1,1 +1,26 @@
 # telephoneNumValidator
+function telephoneCheck(str) {
+  if (str.indexOf("(") === -1 && str.indexOf(")") > -1) {
+    return false;
+  }
+
+  if (str[0] === "-") return false;
+
+  if (str.indexOf(")") - str.indexOf("(") >= 5) {
+    return false;
+  }
+
+  let formattedNum = str.replace(/-| /g, "");
+
+  if (formattedNum.indexOf("(") < formattedNum.indexOf(")")) {
+    formattedNum = formattedNum.replace(/\(|\)/g, "");
+  }
+
+  if (formattedNum.length === 10) {
+    return true;
+  } else if (formattedNum.length === 11 && formattedNum[0] === "1") {
+    return true;
+  } 
+
+  return false;
+}
